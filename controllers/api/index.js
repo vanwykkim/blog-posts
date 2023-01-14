79d51ef32868b452021e7b86179f1950b63270fd
Kim Van Wyk <kmvanwyk@msn.com>
2023-01-14 14:36:29 -0800

    logging in?

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const userRoutes = require("../userRoutes");
-const blogPostRoutes = require("../blogPostRoutes");
-const commentRoutes = require("../commentRoutes");
+const userRoutes = require("../api/userRoutes");
+const blogPostRoutes = require("../api/blogPostRoutes");
+const commentRoutes = require("../api/commentRoutes");
 
 router.use("/users", userRoutes);
 router.use("/blogpost", blogPostRoutes);
