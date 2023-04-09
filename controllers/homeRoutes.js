2ce28848d71935ed90fe0efe45cd1f98e6c93519
Kim Van Wyk <kmvanwyk@msn.com>
2023-04-08 23:09:33 -0700

    updating

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,7 +72,7 @@ console.log(user);
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/homepage');
+    res.redirect('/partial/homepage');
     return;
   }
 
--- public/js/login.js
+++ public/js/login.js
@@ -14,8 +14,8 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/blogpost-details');
+      // If successful, redirect the browser to the homepage page
+      document.location.replace('/partial/homepage');
     } else {
       alert(response.statusText);
     }
